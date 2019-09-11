fe088d1d584d8991b15d81db197ffc2e5d7ed66c
sapanapoudel <sapana.poudel17@outlook.com>
2019-09-11 11:16:46 -0700

    post assigne

--- src/main/java/com/poudel/taskmaster/controller/TaskController.java
+++ src/main/java/com/poudel/taskmaster/controller/TaskController.java
@@ -1,11 +1,16 @@
 package com.poudel.taskmaster.controller;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.poudel.taskmaster.model.Task;
+import com.poudel.taskmaster.repository.DynamoDBConfig;
 import com.poudel.taskmaster.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -22,11 +27,25 @@ public class TaskController {
 
     @PostMapping("/tasks")
     public Task addNewTask(@RequestBody Task task) {
-        Task newTask = new Task();
-        newTask.setTitle(task.getTitle());
-        newTask.setDescription(task.getDescription());
-        newTask.setStatus(task.getStatus());
+        Task newTask = new Task(task.getTitle(), task.getDescription(),
+                task.getAssignee());
+//        newTask.setTitle(task.getTitle());
+//        newTask.setDescription(task.getDescription());
+//        newTask.setStatus(task.getStatus());
+        DynamoDBConfig dynamoDBConfig = new DynamoDBConfig();
+
+        AmazonDynamoDB dynamo = new AmazonDynamoDBClient(dynamoDBConfig.credentialsProvider().
+                getCredentials());
+        DynamoDBMapper  mapper = new DynamoDBMapper(dynamo);
+        mapper.save(newTask);
         taskRepository.save(newTask);
         return newTask;
     }
+
+    @GetMapping("/users/{name}/tasks")
+    public Optional<Task> getTasksOfName(@PathVariable String name){
+        Optional<Task> tasks = taskRepository.findByAssignee(name);
+        return tasks;
+    }
+
 }
